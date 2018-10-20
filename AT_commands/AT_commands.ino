0a9578576e56cc7dac7d3f1f843aca07353b0866
take2rohit <rohitlal789@gmail.com>
2018-10-20 16:59:37 +0530

    Master slave, imu cal,check value

---
+++ AT_commands/AT_commands.ino
@@ -0,0 +1,14 @@
+void setup() {
+  /*  AT+NAME= MASTER
+   *  AT+NAME?
+   *  AT+ROLE = 1 (MASTER)
+   *  AT+UART = 38400,0,0
+   */
+  
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
---
+++ MASTER/MASTER.ino
@@ -0,0 +1,28 @@
+#define ledPin 13
+int state = 0;
+int potValue = 0;
+void setup() {
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, LOW);
+  Serial.begin(38400); // Default communication rate of the Bluetooth module
+}
+void loop() {
+ if(Serial.available() > 0){ // Checks whether data is comming from the serial port
+    state = Serial.read(); // Reads the data from the serial port
+ }
+ // Controlling the LED
+ if (state == '1') {
+  digitalWrite(ledPin, HIGH); // LED ON
+  state = 0;
+ }
+ else if (state == '0') {
+  digitalWrite(ledPin, LOW); // LED ON
+  state = 0;
+ }
+ // Reading the potentiometer
+ potValue = analogRead(A0);
+ int potValueMapped = map(potValue, 0, 1023, 0, 255);
+ Serial.write(potValueMapped); // Sends potValue to servo motor
+ delay(10);
+}
+
